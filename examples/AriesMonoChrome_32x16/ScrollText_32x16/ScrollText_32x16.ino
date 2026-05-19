91a59ce4f59e221a0dffa4997686c2a01cd3cd01
Nishil Patel <nishil.k.patel@gmail.com>
2026-05-19 13:22:01 +0530

    Add scrolling text example for AriesDMD monochrome panels

---
+++ examples/AriesMonoChrome_32x16/ScrollText_32x16/ScrollText_32x16.ino
@@ -0,0 +1,57 @@
+#include "AriesDMD.h"
+#include "AriesFont5x7.h"
+
+AriesDMD dmd(2, 1, 8, 6, 7, 5);   // 2 panels wide, 1 panel high, OE, A, B, Lat
+// Clk --> SPI2_SCLK
+// Data --> SPI2_MOSI
+// GND --> GND
+
+const char text[] = "NISHIL PATEL";
+int scrollX;
+
+void setup() {
+  dmd.begin();
+  scrollX = dmd.width();
+}
+
+// Right to Left
+/* 
+void loop() {
+  dmd.scanDisplay();
+
+  static unsigned long lastMove = 0;
+  if (millis() - lastMove > 70) {
+    lastMove = millis();
+
+    dmd.clearScreen(true);
+    AriesDrawString(dmd, scrollX, 4, text);
+
+    scrollX--;
+    int textWidth = strlen(text) * (ARIES_FONT_WIDTH + 1);
+
+    if (scrollX < -textWidth) {
+      scrollX = dmd.width();
+    }
+  }
+}
+*/ 
+
+// Left to Right 
+void loop() {
+    dmd.scanDisplay();   
+
+    static unsigned long lastMove = 0;
+    if (millis() - lastMove > 40) {
+        lastMove = millis();
+
+        dmd.clearScreen(true);
+        AriesDrawString(dmd, scrollX, 4, text);
+
+        scrollX++;   
+
+        int textWidth = strlen(text) * (ARIES_FONT_WIDTH + 1);
+        if (scrollX > dmd.width()) {
+            scrollX = -textWidth;   
+        }
+    }
+}
