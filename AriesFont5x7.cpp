f63398fabb2ef53e81b8828df2df3f8f903561c7
Nishil Patel <nishil.k.patel@gmail.com>
2026-05-19 13:17:23 +0530

    Implement font drawing functions for AriesDMD

---
+++ AriesFont5x7.cpp
@@ -0,0 +1,79 @@
+#include "AriesFont5x7.h"
+#include "AriesDMD.h"
+
+#include <avr/pgmspace.h>
+
+#define FONT_HEADER_SIZE 6
+
+void AriesDrawChar(
+    AriesDMD& dmd,
+    int x,
+    int y,
+    char c,
+    uint8_t mode
+)
+{
+    if(
+        c < ARIES_FONT_FIRST ||
+        c >= (ARIES_FONT_FIRST + 96)
+    )
+    {
+        return;
+    }
+
+    uint16_t charIndex =
+        c - ARIES_FONT_FIRST;
+
+    uint16_t glyphIndex =
+        FONT_HEADER_SIZE +
+        (charIndex * ARIES_FONT_WIDTH);
+
+    for(uint8_t col = 0;
+        col < ARIES_FONT_WIDTH;
+        col++)
+    {
+        uint8_t line =
+            pgm_read_byte(
+                &System5x7[glyphIndex + col]
+            );
+
+        for(uint8_t row = 0;
+            row < ARIES_FONT_HEIGHT;
+            row++)
+        {
+            if(line & (1 << row))
+            {
+                dmd.writePixel(
+                    x + col,
+                    y + row,
+                    mode,
+                    true
+                );
+            }
+        }
+    }
+}
+
+void AriesDrawString(
+    AriesDMD& dmd,
+    int x,
+    int y,
+    const char* str,
+    uint8_t mode
+)
+{
+    while(*str)
+    {
+        AriesDrawChar(
+            dmd,
+            x,
+            y,
+            *str,
+            mode
+        );
+
+        x += (ARIES_FONT_WIDTH + 1);
+
+        str++;
+    }
+}
