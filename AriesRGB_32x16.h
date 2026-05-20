cff629c8666b0fd4f6dcfd0c018417e18ac96c12
Nishil Patel <nishil.k.patel@gmail.com>
2026-05-20 12:12:31 +0530

    Refactor drawPixel method and rearrange member variables

--- AriesRGB_32x16.h
+++ AriesRGB_32x16.h
@@ -6,7 +6,7 @@
 #define PANEL_WIDTH  32
 #define PANEL_HEIGHT 16
 
-#define MAX_PANELS_X 20   
+#define MAX_PANELS_X 20
 #define MAX_PANELS_Y 5
 
 class AriesRGB_32x16
@@ -23,20 +23,38 @@ public:
 
     void begin();
     void clear();
-    void drawPixel(int x, int y, bool r, bool g, bool b);
+
+    void drawPixel(
+        int x,
+        int y,
+        bool r,
+        bool g,
+        bool b
+    );
+
     void refresh();
 
     uint16_t width();
     uint16_t height();
 
 private:
+
     uint8_t PIN_R1, PIN_G1, PIN_B1;
     uint8_t PIN_R2, PIN_G2, PIN_B2;
-    uint8_t PIN_CLK, PIN_LAT, PIN_OE;
-    uint8_t PIN_A, PIN_B, PIN_C;
 
-    uint8_t panelsWide, panelsHigh;
-    uint16_t totalWidth, totalHeight;
+    uint8_t PIN_CLK;
+    uint8_t PIN_LAT;
+    uint8_t PIN_OE;
+
+    uint8_t PIN_A;
+    uint8_t PIN_B;
+    uint8_t PIN_C;
+
+    uint8_t panelsWide;
+    uint8_t panelsHigh;
+
+    uint16_t totalWidth;
+    uint16_t totalHeight;
 
     bool framebuffer[MAX_PANELS_Y * PANEL_HEIGHT]
                     [MAX_PANELS_X * PANEL_WIDTH][3];
