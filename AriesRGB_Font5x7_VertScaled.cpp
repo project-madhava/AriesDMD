245492f0a7a2a0fe4bdb25cc6aa983c5f5bc562f
Nishil Patel <nishil.k.patel@gmail.com>
2026-05-22 14:58:31 +0530

    Add vertical scaled font drawing functions

---
+++ AriesRGB_Font5x7_VertScaled.cpp
@@ -0,0 +1,96 @@
+#include "AriesRGB_Font5x7_VertScaled.h"
+
+#include "AriesRGB_Font5x7.h"
+#include "AriesRGB_Font5x7_Vert.h"
+
+#include <avr/pgmspace.h>
+
+#define FONT_HEADER_SIZE 6
+
+
+void AriesDrawCharVertScaled(
+    AriesRGB_32x16& display,
+    int x,
+    int y,
+    char c,
+    bool r,
+    bool g,
+    bool b,
+    int scale
+)
+{
+    if (c < ARIES_FONT_FIRST || c >= (ARIES_FONT_FIRST + 96))
+        return;
+
+    uint16_t charIndex =
+        c - ARIES_FONT_FIRST;
+
+    uint16_t glyphIndex =
+        FONT_HEADER_SIZE +
+        (charIndex * ARIES_FONT_WIDTH);
+
+    for(uint8_t col = 0;
+        col < ARIES_FONT_WIDTH;
+        col++)
+    {
+        uint8_t line =
+            pgm_read_byte(
+                &System5x7[glyphIndex + col]
+            );
+
+        for(uint8_t row = 0;
+            row < ARIES_FONT_HEIGHT;
+            row++)
+        {
+            if(line & (1 << row))
+            {
+                
+                for(int sx = 0; sx < scale; sx++)
+                {
+                    for(int sy = 0; sy < scale; sy++)
+                    {
+                        AriesDrawPixelVert(
+                            display,
+
+                            x + (col * scale) + sx,
+                            y + (row * scale) + sy,
+
+                            r, g, b
+                        );
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+void AriesDrawStringVertScaled(
+    AriesRGB_32x16& display,
+    int x,
+    int y,
+    const char* str,
+    bool r,
+    bool g,
+    bool b,
+    int scale
+)
+{
+    while(*str)
+    {
+        AriesDrawCharVertScaled(
+            display,
+            x,
+            y,
+            *str,
+            r, g, b,
+            scale
+        );
+
+        x +=
+            ((ARIES_FONT_WIDTH + 1)
+            * scale);
+
+        str++;
+    }
+}
